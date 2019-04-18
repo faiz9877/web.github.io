9945810a4b9080a66e3ba47920366cb0848170ec
Muhammad Faiz <33045014+faiz9877@users.noreply.github.com>
2019-04-18 23:03:04 +0800

    admin
    tes

---
+++ admin/admin/tambah_masjid.php
@@ -0,0 +1,25 @@
+<aside class="right-side">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Dashboard
+            <small>Control panel</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+    <section class="content">
+
+        <!-- Small boxes (Stat box) -->
+          <form>
+            <div class="form-group">
+              <label for="nama">Nama Masjd</label>
+              <input type="text" name="nama"class="form-control">
+            </div><div class="form-group">
+              <label for="nama">Alamat Masjd</label>
+              <input type="text" name="alamat"class="form-control">
+            </div>
+            <button type="submit" class="btn btn-default bg-blue">Simpan</button>
+          </section>
